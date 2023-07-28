ad4b8d41f0666b3a9bef57009777dde29e49f0dd
dev-manuel <manuel_krofficial@online.de>
2023-07-28 10:01:03 +0200

    feat: finished code gen

--- src/MKAbsynCodeGen.jl
+++ src/MKAbsynCodeGen.jl
@@ -13,107 +13,159 @@ using ExportAll
 
 import ListUtil
 
-const INITIAL_SUB_CLASS_NAME = "context_initial"
+const INITIAL_SUB_CLASS_NAME = "Context_Initial"
 
-function generateCodeFromMKAbsyn(inProgram::MKAbsyn.Program)::Dict{Class,List{ContextEquationSystem}}
 
-    println("a")
+function mockContextDeclarations(traverser::MKAbsynProgramTraverser, inClass::Class)
+    translateContextDefinition(traverser, CONTEXTDEFINITION("sunny", RELATION(
+            CREF(CREF_IDENT("x", Nil{Any}())),
+            GREATER(),
+            INTEGER(2),
+        )), inClass)
+    translateContextDefinition(traverser, CONTEXTDEFINITION("windy", RELATION(
+            CREF(CREF_IDENT("x", Nil{Any}())),
+            LESS(),
+            INTEGER(0),
+        )), inClass)
+end
+
+function generateCodeFromMKAbsyn(inProgram::MKAbsyn.Program)::String
     helper = MKAbsynProgramTraverser()
     resetState(helper)
     a = translateProgram(helper, inProgram)
+
+    #TODO: remove this
+    mockContextDeclarations(helper, ListUtil.first(inProgram.classes))
+
+
+    println("=======TRANSLATED PROGRAM==========")
     println(a)
+    println("===================================")
     context_dict::Dict{Class,List{ContextDeclaration}} = helper.context_dict
     context_equation_dict::Dict{Class,List{ContextEquationSystem}} = helper.context_equation_dict
-    return context_equation_dict
 
     # now context and context equation mappings are full
     # validate contexts and context equation mappings
-    if !validate()
+    if !validate(context_equation_dict, context_dict)
         @error "Contexts not valid"
     end
 
     createModelVariations(inProgram, context_equation_dict, context_dict)
 end
 
-function validate()::Bool
+function validate(context_equation_dict::Dict{Class,List{ContextEquationSystem}}, context_dict::Dict{Class,List{ContextDeclaration}})::Bool
     #todo: check for duplicate labels in context definition
+    #todo: check for duplicate labels in equation sections
     #todo: check for existence of all mentioned contexts in equation sections
     true
 end
 
 
 function createModelVariations(inProgram::MKAbsyn.Program, context_equation_dict::Dict{Class,List{ContextEquationSystem}}, context_dict::Dict{Class,List{ContextDeclaration}})::String
-    out = list()
+    out = []
     for class in inProgram.classes
-        if isa(class.restriction, MKAbsyn.Restriction.R_CLASS)
-            push!(out, MKAbsynProgramTraverser().translateClass(class))
-        else
-            push!(out, createModelVariation(class, context_equation_dict[class], context_dict[class]))
+        @match class.restriction begin
+            MKAbsyn.R_CLASS() => begin
+                push!(out, translateClass(MKAbsynProgramTraverser(), class))
+            end
+            MKAbsyn.R_MODEL() => begin
+                push!(out, createModelVariation(class, context_equation_dict[class], context_dict[class]))
+            end
         end
     end
     join(out, "\n")
 
 end
 
-function createModelVariation(inClass::MKAbsyn.Class, context_equation_sections::List{ContextEquationSystem}, contexts::List{ContextDeclaration})::String
+function createInstanceName(contextName::String)::String
+    lowercasefirst(contextName) + "_instance"
+end
 
+function createModelVariation(inClass::MKAbsyn.Class, context_equation_sections::List{ContextEquationSystem}, contexts::List{ContextDeclaration})::String
+    println("creating model variation for " + inClass.name)
+
+    # check if contexts exist
     if isempty(contexts) || isempty(context_equation_sections)
-        return MKAbsynProgramTraverser().translateClass(class)
+        println("no context or context equation sections for " + inClass.name + " -> skipping variationing")
+        return translateClass(MKAbsynProgramTraverser(), class)
     end
 
-    out = "model " + inClass.name
+    # check if ClassDef is of type PARTS or CLASS_EXTENDS (otherwise no variation possible)
+    canBeVaried = false
+    @match inClass.body begin
+        MKAbsyn.PARTS(__) => begin
+            canBeVaried = true
+        end
+        MKAbsyn.CLASS_EXTENDS(__) => begin
+            canBeVaried = true
+        end
+    end
 
-    # create model for each context
-    models = list()
-    # initial  model
-    initialSubClass = deepcopy(inClass)
-    initialSubClass.name = INITIAL_SUB_CLASS_NAME
-    push!(models, MKAbsynProgramTraverser().translateClass(initialSubClass))
+    if !canBeVaried
+        println("class " + inClass.name + " cannot be varied (no PARTS or CLASS_EXTENDS) -> skipping variationing")
+        return translateClass(MKAbsynProgramTraverser(), inClass)
+    end
+
+
+    # prepare outer model
+    out = "model " + inClass.name + "\n"
+
+
+    # create model list for each context
+    models = []
+
+    # initial  model can be translated without any changes
+    initialClassName = inClass.name + "__" + INITIAL_SUB_CLASS_NAME
+    initialSubClass = instantiateClassVariant(inClass, initialClassName, list())
+    push!(models, translateClass(MKAbsynProgramTraverser(), initialSubClass))
 
     # variations
     contextDict = Dict()
-    counter = 0
     for context in contexts
         equationItems::List{EquationItem} = getEquationItemsForContext(context_equation_sections, context)
         if equationItems == nil
+            #TODO: handle case
             continue
         end
-        model_id = "context_" + counter
-        contextDict[context] = model_id
-        counter += 1
+        model_id = inClass.name + "_" + uppercasefirst(context.label)
+        contextDict[model_id] = context.condition
 
-        subClass = createSubClass(inClass, equationItems, model_id)
-        push!(models, MKAbsynProgramTraverser().translateClass(subClass))
+        subClass = instantiateClassVariant(inClass, model_id, equationItems)
+        push!(models, translateClass(MKAbsynProgramTraverser(), subClass))
     end
 
+    # add structural mode declarations for initial and contexts
+    out += "structuralMode " + initialClassName + " " + createInstanceName(initialClassName) + ";\n"
+    out += join(map(x -> "structuralMode " + x + " " + createInstanceName(x) + ";\n", collect(keys(contextDict))), "")
 
-    out += join(map(x -> "structuralMode " + x, collect(values(contextDict)), "\n")) + "\n"
+    # add sub models
+    out += join(models, "\n") + "\n"
 
-    out += join(models, "\n")
-
+    # add new equation section
+    out += "equation\n"
 
     # add initialStructureMode
-    out += "initialStructuralState(" + INITIAL_SUB_CLASS_NAME + ")\n"
+    out += "initialStructuralState(" + createInstanceName(initialClassName) + ");\n"
 
-    # add structuralTransition
-    for (from_context, from_label) in pairs(contextDict)
-        for (to_context, to_label) in pairs(contextDict)
-            if from_label == to_label
+    # add transitions
+    for (from_context, _) in pairs(contextDict)
+        for (to_context, condition) in pairs(contextDict)
+            if from_context == to_context
                 continue
             end
-            out += "structuralTransition(" + from_label + "," + to_label + "," + MKAbsynProgramTraverser().translateExpression(to_context.condition) + ")\n"
+            out += "structuralTransition(" + createInstanceName(from_context) + ", " + createInstanceName(to_context) + ", " + translateExpression(MKAbsynProgramTraverser(), condition) + ");\n"
         end
     end
 
     # finish super model
-    out += "end " + inClass.name
-
+    out += "end " + inClass.name + ";\n"
+    out
 end
 
 function getEquationItemsForContext(context_equation_sections::List{ContextEquationSystem}, context::ContextDeclaration)::List{EquationItem}
     for context_equation_section in context_equation_sections
-        if context_equation_section.label == context.label
-            return context_equation_section.contents
+        if context_equation_section.contextLabel == context.label
+            return context_equation_section.equations
         end
     end
     nil
@@ -121,15 +173,64 @@ end
 
 
 
-function createSubClass(originalClass::Class, equationItems::List{EquationItem}, name::String)::Class
-    # create copy of class
-    curClass = deepcopy(originalClass)
-    curClass.name = name
-    #todo: check if body is PARTS
+
+function instantiateClassVariant(originalClass::Class, name::String, equationItems::List{EquationItem}=nothing)::Class
+
+    newClassDef = originalClass.body
+    if !isnothing(equationItems) && !isempty(equationItems)
+        # remove all EQUATIONS from curClass.body
+        newClassDef = replaceEquationsInClassDef(originalClass.body, equationItems)
+    end
+
+
+    newClass = CLASS(name, originalClass.partialPrefix, originalClass.finalPrefix, originalClass.encapsulatedPrefix, originalClass.restriction, newClassDef, originalClass.info)
     # todo: check every classPart if it is EQUATIONS
 
-    curClass.body.classParts
 
+    newClass
+
+end
+
+function replaceEquationsInClassDef(classDef::ClassDef, equationItems::List{EquationItem})::ClassDef
+    @match classDef begin
+        MKAbsyn.PARTS(
+            typeVars=cTypeVars,
+            classAttrs=cAttrs,
+            classParts=cParts,
+            ann=cAnn,
+            comment=cComment
+        ) => begin
+            MKAbsyn.PARTS(cTypeVars, cAttrs, replaceEquationsInClassParts(cParts, equationItems), cAnn, cComment)
+        end
+        MKAbsyn.CLASS_EXTENDS(
+            baseClassName=cBaseClassName,
+            modifications=cModifications,
+            comment=cComment,
+            parts=cParts,
+            ann=cAnn
+        ) => begin
+            MKAbsyn.CLASS_EXTENDS(cBaseClassName, cModifications, cComment, replaceEquationsInClassParts(cParts, equationItems), cAnn)
+        end
+    end
+end
+
+
+function replaceEquationsInClassParts(classParts::List{ClassPart}, equationItems::List{EquationItem})::List{ClassPart}
+    out::List{ClassPart} = list()
+    for classPart in classParts
+        @match classPart begin
+            MKAbsyn.EQUATIONS(__) => begin end
+            MKAbsyn.CONTEXTEQUATIONS(__) => begin end
+            _ => begin
+                # keep all other classParts
+                out = ListUtil.append_reverse(out, ListUtil.create(convert(ClassPart, classPart)))
+            end
+        end
+    end
+    a::ClassPart = convert(ClassPart, MKAbsyn.EQUATIONS(equationItems))
+    l::List{ClassPart} = ListUtil.create(a)
+    out = ListUtil.append_reverse(out, l)
+    out
 
 end
 
